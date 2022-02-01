53938284bd2c14894660c352202072ff8a0a03d6
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-01 19:50:17 +0530

    for nested array

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import cars from "./Cars";
 
 export default function App() {
   const [dog, cat] = animals;
-  const { name, sound } = dog;
-  console.log(sound);
+  // const { name, sound } = dog;
+  // console.log(sound);
   //also useful
   // const {name:dogName, sound:dogSound} = dog;
   // console.log(dogSound);
@@ -13,5 +13,14 @@ export default function App() {
   //if value is not defined in data then use default value
   // const {name="Tommy", sound="woof"} = dog;
   // console.log(dogSound);
+
+  //when you get nested array
+  const {
+    name,
+    sound,
+    requirement: { food, water }
+  } = dog;
+  console.log(food + water);
+
   return <div className="App"></div>;
 }
